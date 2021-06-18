ca8536d5508cb07d8e958173dd1ff8cacab55d44
matheusgomes062 <matheusgomes062@gmail.com>
2021-06-17 21:33:28 -0300

    feat: started styling and first page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,42 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'covid-19-infoedu',
+    title: "covid-19-infoedu",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ["@nuxtjs/style-resources"],
+  styleResources: {
+    scss: [
+      "assets/scss/setup/_default.scss",
+      "assets/scss/setup/_grid.scss",
+      "assets/scss/setup/_mixins.scss",
+      "assets/scss/setup/_reset.scss",
+      "assets/scss/setup/_variables.scss"
+    ]
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,90 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        covid-19-infoedu
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="titleAndPictureContainer">
+        <div class="titleAndInfoContainer">
+          <h2>188+ Países afetados pelo</h2>
+          <h1>CORONAVÍRUS</h1>
+          <section>
+            O vírus foi inicialmente reportado em Wuhan, Hubel China em 17 de
+            Novembro de 2019, e em 11 de Março de 2020 a Organização Mundial da
+            Saúde (OMS) declarou o coronavírus como uma pandemia.
+          </section>
+        </div>
+        <div class="imageContainer"></div>
+      </div>
+      <div class="cardsContainer">
+        <div class="card">
+          <p>9999+</p>
+          <p>Total de mortes</p>
+        </div>
+        <div class="card">
+          <p>9999+</p>
+          <p>Total de mortes</p>
+        </div>
+        <div class="card">
+          <p>9999+</p>
+          <p>Total de mortes</p>
+        </div>
+        <div class="card">
+          <p>9999+</p>
+          <p>Total de mortes</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style lang="scss">
+.titleAndPictureContainer {
   display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.titleAndInfoContainer {
+  display: flex;
+  flex-direction: column;
+  h2,
+  h1 {
+    margin: 10px 0;
+  }
+  p {
+    margin-top: 40px;
+  }
+}
+
+.imageContainer {
+  min-width: 500px;
+  min-height: 500px;
+  background-color: $default-gray;
+  border-radius: 999999999px;
+}
+
+.cardsContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 130px;
+  padding: 10px;
+  cursor: pointer;
   justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background-color: $default-gray;
+  border-radius: 10px;
+  p {
+    text-align: center;
+    font-weight: 700;
+    margin: 5px;
+  }
 }
 </style>
