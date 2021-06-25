1c30e405b2a2c92365f90c84e67298ef9c9c2e8b
matheusgomes062 <matheusgomes062@gmail.com>
2021-06-24 22:46:01 -0300

    feat: added healthInSports page

--- pages/healthInSports.vue
+++ pages/healthInSports.vue
@@ -15,15 +15,41 @@ export default {
       info:
         "Durante a pandemia do novo coronavírus, diante das medidas de distanciamento social, a maioria dos praticantes de atividade física passou a realizar seus treinamentos dentro de casa, adaptando também essa parte da vida aos novos tempos. ",
       tips: [
-        {
-          title: "teste 1"
-        },
-        {
-          title: "teste 2"
-        },
-        {
-          title: "teste 3"
-        }
+        [
+          {
+            title: "Faça atividade física de maneira regular",
+            link:
+              "https://g1.globo.com/bemestar/coronavirus/noticia/2020/03/20/coronavirus-como-manter-a-rotina-de-atividades-fisicas-mesmo-em-quarentena.ghtml"
+          },
+          {
+            title: "Evite esportes de contato",
+            link:
+              "https://www.em.com.br/app/noticia/bem-viver/2020/09/02/interna_bem_viver,1181701/que-esportes-oferecem-maior-ou-menor-risco-de-contagio-na-pandemia.shtml"
+          }
+        ],
+        [
+          {
+            title: "Defina objetivos adequados",
+            link:
+              "https://www.uol.com.br/vivabem/colunas/paola-machado/2021/06/11/5-dicas-para-o-retorno-as-atividades-esportivas.htm"
+          },
+          {
+            title: "Defina um cronograma adequado",
+            link:
+              "https://www.uol.com.br/vivabem/colunas/paola-machado/2021/06/11/5-dicas-para-o-retorno-as-atividades-esportivas.htm"
+          }
+        ],
+        [
+          {
+            title: "Esportes ao ar livre são a melhor escolha",
+            link:
+              "https://www.folhadelondrina.com.br/saude/cinco-atividades-fisicas-para-manter-o-ritmo-na-pandemia-3040117e.html"
+          },
+          {
+            title: "Crie um caderno de rotinas esportivas",
+            link: "https://impulsiona.org.br/cadernos-rotina-quarentena/"
+          }
+        ]
       ]
     };
   },
