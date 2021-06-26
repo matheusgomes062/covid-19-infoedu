9e6fa478045f81e09e339eba75d1a9ab78440d54
matheusgomes062 <matheusgomes062@gmail.com>
2021-06-26 14:03:23 -0300

    feat: added all links and tips

--- pages/companyFinances.vue
+++ pages/companyFinances.vue
@@ -15,15 +15,46 @@ export default {
       info:
         "Com a nova alta de casos e internações por Covid-19, a recuperação econômica esperada para 2021 precisará ser adiada. Empreendedores nem bem se recuperaram do baque de 2020 e já precisam lidar com novas regras de restrição de funcionamento em datas importantes para o varejo, como Páscoa e, possivelmente, Dia das Mães.",
       tips: [
-        {
-          title: "teste 1"
-        },
-        {
-          title: "teste 2"
-        },
-        {
-          title: "teste 3"
-        }
+        [
+          {
+            title: "Faça o orçamento na ponta do lápis",
+            link:
+              "https://www.serasa.com.br/ensina/suas-economias/coronavirus-10-dicas/"
+          },
+          {
+            title: "Na crise, corte despesas!",
+            link:
+              "https://www.em.com.br/app/noticia/bem-viver/2020/09/02/interna_bem_viver,1181701/que-esportes-oferecem-maior-ou-menor-risco-de-contagio-na-pandemia.shtml"
+          },
+          {
+            title: "Renegocie gastos essenciais",
+            link:
+              "https://www.serasa.com.br/ensina/suas-economias/coronavirus-10-dicas/"
+          }
+        ],
+        [
+          {
+            title: "Estipule limites de gastos mensais",
+            link:
+              "https://agenciafiep.com.br/2020/05/15/seis-dicas-para-organizar-melhor-as-suas-financas-durante-pandemia/"
+          },
+          {
+            title: "Deixe o cheque especial para último caso",
+            link:
+              "https://agenciafiep.com.br/2020/05/15/seis-dicas-para-organizar-melhor-as-suas-financas-durante-pandemia/"
+          }
+        ],
+        [
+          {
+            title: "Corte gastos supérfluos",
+            link: "https://exal.com.br/financas-pessoais/"
+          },
+          {
+            title: "Tenha autocontrole!",
+            link:
+              "https://idec.org.br/idec-na-imprensa/guia-para-o-bolso-dicas-para-financas-pessoais-durante-pandemia"
+          }
+        ]
       ]
     };
   },
--- pages/personalFinances.vue
+++ pages/personalFinances.vue
@@ -15,15 +15,46 @@ export default {
       info:
         "A crise causada pela pandemia de Covid-19 pegou muitas empresas, empresários e trabalhadores de surpresa, tanto emocionalmente quanto financeiramente. Uma pesquisa realizada pela global Willis Towers Watson mostra que a preocupação com a situação financeira afeta negativamente a saúde e o bem-estar dos colaboradores, bem como seu engajamento no trabalho.",
       tips: [
-        {
-          title: "teste 1"
-        },
-        {
-          title: "teste 2"
-        },
-        {
-          title: "teste 3"
-        }
+        [
+          {
+            title: "Faça o orçamento na ponta do lápis",
+            link:
+              "https://www.serasa.com.br/ensina/suas-economias/coronavirus-10-dicas/"
+          },
+          {
+            title: "Na crise, corte despesas!",
+            link:
+              "https://www.em.com.br/app/noticia/bem-viver/2020/09/02/interna_bem_viver,1181701/que-esportes-oferecem-maior-ou-menor-risco-de-contagio-na-pandemia.shtml"
+          },
+          {
+            title: "Renegocie gastos essenciais",
+            link:
+              "https://www.serasa.com.br/ensina/suas-economias/coronavirus-10-dicas/"
+          }
+        ],
+        [
+          {
+            title: "Estipule limites de gastos mensais",
+            link:
+              "https://agenciafiep.com.br/2020/05/15/seis-dicas-para-organizar-melhor-as-suas-financas-durante-pandemia/"
+          },
+          {
+            title: "Deixe o cheque especial para último caso",
+            link:
+              "https://agenciafiep.com.br/2020/05/15/seis-dicas-para-organizar-melhor-as-suas-financas-durante-pandemia/"
+          }
+        ],
+        [
+          {
+            title: "Corte gastos supérfluos",
+            link: "https://exal.com.br/financas-pessoais/"
+          },
+          {
+            title: "Tenha autocontrole!",
+            link:
+              "https://idec.org.br/idec-na-imprensa/guia-para-o-bolso-dicas-para-financas-pessoais-durante-pandemia"
+          }
+        ]
       ]
     };
   },
